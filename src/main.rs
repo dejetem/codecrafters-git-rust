6ba601d982d858428659bb869f0f2622ea61898d
dejetem <enyeoluwarotimisimon@gmail.com>
2025-03-20 22:24:45 +0100

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use std::env;
 #[allow(unused_imports)]
 use std::fs;
 
+use flate2::read::ZlibDecoder;
+use std::io::prelude::*;
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     eprintln!("Logs from your program will appear here!");
@@ -15,6 +18,13 @@ fn main() {
         fs::create_dir(".git/refs").unwrap();
         fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
         println!("Initialized git directory")
+    } else if args[1] == "cat-file" {
+        let content =
+            fs::read(format!(".git/objects/{}/{}", &args[3][..2], &args[3][2..])).unwrap();
+        let mut z = ZlibDecoder::new(&content[..]);
+        let mut s = String::new();
+        z.read_to_string(&mut s).unwrap();
+        print!("{}", &s[8..]);
     } else {
         println!("unknown command: {}", args[1])
     }
